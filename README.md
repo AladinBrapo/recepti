d111c3e1add4b3b941acd40117eeadea5ac7d91d
AladinBrapo <169046176+AladinBrapo@users.noreply.github.com>
2024-09-10 10:53:06 +0200

    updated index and readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # recepti
+Na tej spletni strani bodo bili recepti. Tukaj bodo ljudje lahko najdli in tudi dodali svoje recepte.
--- index.php
+++ index.php
@@ -1,11 +1,49 @@
+<?php include_once 'seja.php'; ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="sl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
+    <title>Techy trgovina</title>
+    
+    <link rel="stylesheet" href="css_files/header.css">
+    <link rel="stylesheet" href="css_files/main.css">
+    <link rel="icon" href="slike/icon.png" type="image/png">
+    
+    <style>
+        #gif-image {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
 </head>
 <body>
-    <h2>Helllo</h2>
+    <?php include 'header.php'; //header ?>
+
+    <div id="container">
+        <img id="gif-image" src="slike/TECHY.gif" alt="naslovni GIF">
+        <img id="gif-image-mobile" src="slike/TECHY-telefon.gif" alt="naslovni GIF za telefone" style="display: none;">
+        <!-- Produkti z sql pobrani-->
+    </div>
+    
+    <?php include 'footer.php'; //footer ?>
+
+    <script>
+        var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+        var gifImage = document.getElementById('gif-image');
+        var gifImageMobile = document.getElementById('gif-image-mobile');
+
+        // Preveri ali gre za telefon in prikaži ustrezno sliko
+        if (screenWidth <= 600) {
+            gifImage.style.display = 'none';
+            gifImageMobile.style.display = 'block';
+        } else {
+            gifImage.style.display = 'block';
+            gifImageMobile.style.display = 'none';
+        }
+    </script>
 </body>
 </html>
