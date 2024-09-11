5a4dc24087ee61ee8ec7c9321b69eaa3dab6128a
AladinBrapo <169046176+AladinBrapo@users.noreply.github.com>
2024-09-11 10:08:52 +0200

    created index1 page where the actual page will be and how it looks

---
+++ index1.php
@@ -0,0 +1,23 @@
+<?php include_once 'seja.php'; ?>
+
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Slastne ideje</title>
+</head>
+<body>
+    <?php include 'header.php'; //header ?>
+
+    <div id="container"> 
+
+        <div id="header-text">
+
+        </div>
+    </div>
+    
+    <?php include 'footer.php'; //footer ?>
+</body>
+</html>
